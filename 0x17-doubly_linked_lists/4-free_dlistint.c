d4d1371f0437a4a620aaec399b8e1a5fc1b1a06b
walid salmi <salamiwalid1999@gmail.com>
2023-08-18 13:09:21 -0700

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,26 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
- * @head: pointer to the head of the list
- * @index: index of the node to returned
- * Return: node at index @index, or
- * if the node does not exist, return NULL
+ * free_dlistint - free a list
+ * @head: list
+ * Return: Void
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *current_node = head;
-	unsigned int i = 0;
+	dlistint_t *start, *check;
 
-	if (!head)
-		return (NULL);
-
-	while (current_node)
+	start = head;
+	while (start != NULL)
 	{
-		if (i == index)
-			return (current_node);
-
-		current_node = current_node->next;
-		i++;
+		check = start->next;
+		free(start);
+		start = check;
 	}
-	return (NULL);
+}
